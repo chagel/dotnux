657c6ea5bedf41c7347c186bca95b5871e6ab2ee
MGC <chagel@gmail.com>
2025-03-29 16:07:08 -0700

    chore: hyprland and others

--- configs/fish/user.fish
+++ configs/fish/user.fish
@@ -1,5 +1,10 @@
+fish_add_path $HOME/Dotfiles/scripts
 abbr install sudo pacman -S
 abbr uninstall sudo pacman -Rns
 abbr upgrade-system sudo pacman -Syu
 abbr upgrade-aur yay -Syu --devel --timeupdate
 abbr nap systemctl suspend
+
+if status is-login; and test -z "$DISPLAY"; and test (tty) = "/dev/tty1"
+  exec Hyprland
+end
