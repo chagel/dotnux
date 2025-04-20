010299594d5952af8e6ccb5894593232debdedec
MGC <chagel@gmail.com>
2025-04-20 13:55:43 -0700

    Feat: Improve Fish and Waybar configuration with key loading and styling
    - Added function to load API keys in Fish shell when interactive.
    - Updated Waybar styles for better background and button styles.
    - Modified monitor configuration in Hyprland setup.

--- configs/fish/user.fish
+++ configs/fish/user.fish
@@ -1,10 +1,28 @@
 fish_add_path $HOME/Dotfiles/scripts
+fish_add_path $HOME/.local/bin
+
 abbr install sudo pacman -S
 abbr uninstall sudo pacman -Rns
 abbr upgrade-system sudo pacman -Syu
 abbr upgrade-aur yay -Syu --devel --timeupdate
 abbr nap systemctl suspend
 
+# API Keys
+function load_api_keys --description "Load API keys from password store(1Password)"
+    if type -q pass
+        set -gx OPENAI_API_KEY (op item get cfx7ecmfy3pc7a3zcc7vr2fmhe --reveal --fields credential || echo "")
+        set -gx ANTHROPIC_API_KEY (op item get kyapm6qumqcvjzzmtqvvypxmi4 --reveal --fields credential || echo "")
+    end
+end
+
+# Only load keys in interactive sessions
+if status is-interactive
+	load_api_keys
+end
+
+# Initialize rbenv
+status --is-interactive; and rbenv init - --no-rehash fish | source
+    
 if status is-login; and test -z "$DISPLAY"; and test (tty) = "/dev/tty1"
   exec Hyprland
 end
